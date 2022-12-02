75c7cecbe2436eb3a2fcded8a3d7e4f1725a3c4d
pap3rhat <hschweizer@techfak.uni-bielefeld.de>
2022-12-02 10:21:47 +0100

    Add quit button that closes application

---
+++ visu-tools/Assets/Scripts/World Control Scripts/QuitApplication.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitApplication : MonoBehaviour
+{
+    /* Quits the application */
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
